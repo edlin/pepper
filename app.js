38657e7f9ed3b802ec45a39bc0fc9f60cb9d5cf6
Edward Lin <ed@luxdelux.com>
2012-01-03 12:26:52 -0800

    added template for each page

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 
 var express = require('express')
   , routes = require('./routes')
+  ;
 
 var app = module.exports = express.createServer();
 
@@ -35,6 +36,17 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
+app.get('/encounter', routes.encounter);
+app.get('/engagement', routes.engagement);
+app.get('/fun_facts', routes.fun_facts);
+app.get('/guys', routes.guys);
+app.get('/gals', routes.gals);
+app.get('/location', routes.location);
+app.get('/accommodations', routes.accommodations);
+app.get('/rsvp', routes.rsvp);
+app.post('/rsvp', routes.rsvp_post);
+app.get('/registry', routes.registry);
+app.get('/photos', routes.photos);
 
 app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,77 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' })
 };
+
+
+/*                                                                              
+ * GET story page.                                                              
+ */
+
+exports.encounter = function(req, res){
+  res.render('story/encounter', { title: 'Express' })
+};
+
+exports.engagement = function(req, res){
+  res.render('story/engagement', { title: 'Express' })
+};
+
+exports.fun_facts = function(req, res){
+  res.render('story/fun_facts', { title: 'Express' })
+};
+
+
+/*                                                                              
+ * GET party page.                                                              
+ */
+
+exports.guys = function(req, res){
+  res.render('party/guys', { title: 'Express' })
+};
+
+exports.gals = function(req, res){
+  res.render('party/gals', { title: 'Express' })
+};
+
+
+/*                                                                              
+ * GET details page.                                                            
+ */
+
+exports.location = function(req, res){
+  res.render('details/location', { title: 'Express' })
+};
+
+exports.accommodations = function(req, res){
+  res.render('details/accommodations', { title: 'Express' })
+};
+
+
+/*                                                                              
+ * GET rsvp page.                                                               
+ */
+
+exports.rsvp = function(req, res){
+  res.render('rsvp', { title: 'Express' })
+};
+
+exports.rsvp_post = function(req, res){
+  res.render('rsvp', { title: 'Express' })
+};
+
+
+/*                                                                              
+ * GET registry page.                                                           
+ */
+
+exports.registry = function(req, res){
+  res.render('registry', { title: 'Express' })
+};
+
+
+/*                                                                              
+ * GET photo album page.                                                        
+ */
+
+exports.photos = function(req, res){
+  res.render('photos', { title: 'Express' })
+};
